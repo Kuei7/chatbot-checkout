259b5fcd1e9cf982107b012c7a23753bb0788dba
GustavoHOS <gustavoghkuei@gmail.com>
2025-08-07 00:40:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's3.typebot.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
